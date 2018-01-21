bdc861cb177ad9833c16ceb3ed324fa07ec35da4
Ali Shabani <pyramidalish@gmail.com>
2018-01-21 22:00:29 +0330

    add str_start and helpers file

---
+++ src/helpers.php
@@ -0,0 +1,17 @@
+<?php
+
+if (! function_exists('str_start')) {
+    /**
+     * Begin a string with a single instance of a given value.
+     *
+     * @param  string  $value
+     * @param  string  $prefix
+     * @return string
+     */
+    function str_start($value, $prefix)
+    {
+        $quoted = preg_quote($prefix, '/');
+
+        return $prefix.preg_replace('/^(?:'.$quoted.')+/u', '', $value);
+    }
+}
