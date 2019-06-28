207cdb4031e61a43807837b93ece31bbce896df1
Ali Shabani <pryamidalish@gmail.com>
2019-06-28 12:27:43 +0430

    update callbackQuery to send aswer

--- src/Update/CallbackQuery.php
+++ src/Update/CallbackQuery.php
@@ -5,6 +5,7 @@ namespace Alish\Telegram\Update;
 
 use Alish\Telegram\API\Chat;
 use Alish\Telegram\API\User;
+use Alish\Telegram\Facades\Telegram;
 use Alish\Telegram\TelegramMethods;
 
 class CallbackQuery extends Base
@@ -19,6 +20,16 @@ class CallbackQuery extends Base
     protected function callbackQuery() : \Alish\Telegram\API\CallbackQuery
     {
         return $this->update->getCallbackQuery();
+    }
+
+    /**
+     * Unique identifier for this query
+     *
+     * @return string
+     */
+    protected function id() : string
+    {
+        return $this->callbackQuery()->getId();
     }
 
     /**
@@ -66,5 +77,23 @@ class CallbackQuery extends Base
     protected function chatId() : int
     {
         return $this->chat()->getId();
+    }
+
+    /**
+     * Use this method to send answers to callback queries sent from inline keyboards.
+     * The answer will be displayed to the user as a notification at the top of the chat screen or as an alert. On success, True is returned.
+     *
+     * @param  string  $text
+     * @param  array|null  $options
+     * @return mixed
+     */
+    protected function answer(string $text, ?array $options = [])
+    {
+        $data = array_merge([
+            'callback_query_id' => $this->id(),
+            'text' => $text
+        ], $options);
+
+        return Telegram::answerCallbackQuery($data);
     }
 }
